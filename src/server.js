f3bece2803d3e284b226853f723ca7aeb671c67b
xmikebx <xmikebx@gmail.com>
2024-02-13 11:48:17 +0000

    enhancement: add basic server

---
+++ src/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+
+const port = process.env.PORT || 5002;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "API is healthy" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
