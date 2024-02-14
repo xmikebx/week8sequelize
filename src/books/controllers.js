59f16b2b1bd1e4a0e694a22d9b7d2ea751734bd8
xmikebx <xmikebx@gmail.com>
2024-02-14 09:43:22 +0000

    enhancement: added put delete routes

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -1,4 +1,7 @@
+const sequelize = require("../db/connection");
 const Book = require("./model");
+
+////// ADD BOOK /////
 
 const addBook = async (req, res) => {
   try {
@@ -13,6 +16,89 @@ const addBook = async (req, res) => {
   }
 };
 
+///// GET ALL BOOKS /////
+
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.findAll({});
+    console.log("Route: ", req.path);
+    res.send({ message: "all the books", books: books });
+  } catch (error) {
+    res.send({ message: "its gone pete tong", error: error });
+  }
+};
+
+///// GET A BOOK /////
+
+const getABook = async (req, res) => {
+  try {
+    const books = await Book.findOne({
+      where: { title: req.body.title },
+      rejectOnEmpty: true,
+    });
+
+    console.log("Route: ", req.path);
+    res.send({ message: "the book", books: books });
+  } catch (error) {
+    res.send({ message: "its gone pete tong", error: error });
+  }
+};
+
+///// UPDATE A BOOKS AUTHOR BY TITLE /////
+
+const updateBook = async (req, res) => {
+  try {
+    const update = await Book.update(
+      { author: req.body.author },
+      { where: { title: req.body.title } }
+    );
+    console.log("book updated:", update);
+    res.send({ message: "book updated", update: update });
+  } catch (error) {
+    res.send({ message: "its gone pete tong", error: error });
+  }
+};
+
+///// DELETE A BOOK BY TITLE /////
+
+const deleteBook = async (req, res) => {
+  try {
+    const bookToDestroy = await Book.destroy({
+      where: {
+        title: req.body.title,
+      },
+    });
+
+    console.log("book deleted:", bookToDestroy);
+    res.send({ message: "book deleted", bookToDestroy: bookToDestroy });
+  } catch (error) {
+    res.send({ message: "its gone pete tong", error: error });
+  }
+};
+
+///// DELETE ALL BOOKS //////
+
+const deleteAllBooks = async (req, res) => {
+  try {
+    const destroyAllBooks = await sequelize.destroyAll({
+      where: {},
+    });
+
+    console.log("all books deleted:", destroyAllBooks);
+    res.send({
+      message: "all books deleted",
+      destroyAllBooks: destroyAllBooks,
+    });
+  } catch (error) {
+    res.send({ message: "its gone pete tong", error: error });
+  }
+};
+
 module.exports = {
   addBook: addBook,
+  getAllBooks: getAllBooks,
+  updateBook: updateBook,
+  deleteBook: deleteBook,
+  getABook: getABook,
+  deleteAllBooks: deleteAllBooks,
 };
--- src/books/routes.js
+++ src/books/routes.js
@@ -2,7 +2,22 @@ const { Router } = require("express");
 const bookRouter = Router();
 
 const { addBook } = require("./controllers");
+const { getAllBooks } = require("./controllers");
+const { updateBook } = require("./controllers");
+const { deleteBook } = require("./controllers");
+const { getABook } = require("./controllers");
 
 bookRouter.post("/books/addBook", addBook);
+
+bookRouter.get("/books/getAllBooks", getAllBooks);
+bookRouter.get("/books/getABook", getABook);
+bookRouter.put("/books/updateBook", updateBook);
+bookRouter.delete("/books/deleteBook", deleteBook);
+
+// get all books
+
+// update book author
+
+// delete a single book by title
 
 module.exports = bookRouter;
